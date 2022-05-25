0246a022c928a56e00e7c24828e7dbe8a8690a20
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-05-26 02:18:43 +0300

    Update botcontrols.py

--- iqthon/assistant/botcontrols.py
+++ iqthon/assistant/botcontrols.py
@@ -1,10 +1,7 @@
 from datetime import datetime
 from math import floor
-
 from telethon.utils import get_display_name
-
-from userbot import iqthon
-
+from iqthon import iqthon
 from ..Config import Config
 from ..core.logger import logging
 from ..helpers import reply_id
