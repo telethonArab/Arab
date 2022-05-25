067eac1b279095e970e2516cefd1a425e43209ba
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-05-26 02:36:00 +0300

    Update helpers.py

--- iqthon/core/helpers.py
+++ iqthon/core/helpers.py
@@ -6,7 +6,7 @@ from telethon.utils import get_display_name
 
 from .events import NewMessage
 
-LOGGER = logging.getLogger("userbot")
+LOGGER = logging.getLogger("iqthon")
 
 
 def printUser(entity: types.User) -> None:
