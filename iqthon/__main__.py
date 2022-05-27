edc91bf361db8335f97ba37f32159a2bc99a94bc
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-05-27 06:21:01 +0300

    Update __main__.py

--- iqthon/__main__.py
+++ iqthon/__main__.py
@@ -6,21 +6,14 @@ from .Config import Config
 from .core.logger import logging
 from .core.session import iqthon
 from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
-LOGS = logging.getLogger(
-"تليثون العرب"
-)
-
+LOGS = logging.getLogger("تليثون العرب")
 cmdhr = Config.COMMAND_HAND_LER
 try:
-    LOGS.info(
-"بدء تنزيل تليثون العرب"
-)
-    iqthon.loop.run_until_complete(
-setup_bot())
+    LOGS.info("بدء تنزيل تليثون العرب")
+    iqthon.loop.run_until_complete(setup_bot())
     LOGS.info("بدء تشغيل البوت")
 except Exception as e:
-    LOGS.error(
-f"{str(e)}")
+    LOGS.error(f"{str(e)}")
     sys.exit()
 class CatCheck:
     def __init__(self):
@@ -31,9 +24,7 @@ async def startup_process():
     await load_plugins("plugins")
     await load_plugins("assistant")
     await load_plugins("MusicTelethon")
-    print(
-f"<b> ⌔︙ اهلا بك لقد نصبت تليثون العرب بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/iqthon "
-)
+    print(f"<b> ⌔︙ اهلا بك لقد نصبت تليثون العرب بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/iqthon ")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
@@ -52,9 +43,7 @@ def start_bot():
     return False
 Checker = start_bot()
 if Checker == False:
-    print(
-"عذرا لديك حظر مؤقت حاول التنصيب غدا او بعد 24 ساعة"
-)
+    print("عذرا لديك حظر مؤقت حاول التنصيب غدا او بعد 24 ساعة")
     iqthon.disconnect()
     sys.exit()
 if len(sys.argv) not in (1, 3, 4):
