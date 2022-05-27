f01042c5cc0183941b9e5a7bd4d4c90db5efe833
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-05-27 06:23:08 +0300

    Update __init__.py

--- iqthon/__init__.py
+++ iqthon/__init__.py
@@ -15,7 +15,7 @@ bot = iqthon
 StartTime = time.time()
 catversion = "7.6"
 if Config.UPSTREAM_REPO == "TelethonAr":
-    UPSTREAM_REPO_URL = "https://github.com/telethonArab/telethonAr"
+    UPSTREAM_REPO_URL = "https://github.com/telethonArab/TelethonAr1"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 if Config.PRIVATE_GROUP_BOT_API_ID == 0:
