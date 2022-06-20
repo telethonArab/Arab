a41668e45f0086a7918838be6a897d841407ca07
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-06-20 15:00:57 +0300

    Update warns.py

--- Arab/plugins/warns.py
+++ Arab/plugins/warns.py
@@ -1,5 +1,5 @@
 import html
-from iqthon import iqthon
+from Arab import iqthon
 from ..core.managers import edit_or_reply
 from ..sql_helper import warns_sql as sql
 @iqthon.on(admin_cmd(pattern="تحذير(?:\s|$)([\s\S]*)"))
