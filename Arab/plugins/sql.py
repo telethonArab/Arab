2d48d7de0d2256765989507e0df21c34d2d023f2
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-06-20 15:01:16 +0300

    Update sql.py

--- Arab/plugins/sql.py
+++ Arab/plugins/sql.py
@@ -1,7 +1,7 @@
 import asyncio
 from datetime import datetime
 from telethon.tl import functions, types
-from iqthon import iqthon
+from Arab import iqthon
 from ..Config import Config
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
