2979e20adccb007370c00a2ae026e069bfe99734
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-06-20 14:14:58 +0300

    Update botmanagers.py

--- Arab/assistant/botmanagers.py
+++ Arab/assistant/botmanagers.py
@@ -3,7 +3,7 @@ from math import floor
 
 from telethon.utils import get_display_name
 
-from iqthon import iqthon
+from Arab import iqthon
 
 from ..Config import Config
 from ..core.logger import logging
