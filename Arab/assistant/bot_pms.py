faea77da9986d6f0aa465b475f58e2f9005f4f6e
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-06-20 14:14:20 +0300

    Update bot_pms.py

--- Arab/assistant/bot_pms.py
+++ Arab/assistant/bot_pms.py
@@ -8,7 +8,7 @@ from telethon.errors import UserIsBlockedError
 from telethon.events import CallbackQuery, StopPropagation
 from telethon.utils import get_display_name
 
-from iqthon import Config, iqthon
+from Arab import Config, iqthon
 
 from ..core import check_owner, pool
 from ..core.logger import logging
