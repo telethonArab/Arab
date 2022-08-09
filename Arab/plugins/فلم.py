09c00b10ea2211c01696371502132ddc4619bc81
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-08-09 15:12:18 +0300

    Update فلم.py

--- Arab/plugins/فلم.py
+++ Arab/plugins/فلم.py
@@ -13,14 +13,6 @@ from datetime import datetime
 from ..core.managers import edit_delete, edit_or_reply
 
 
-@iqthon.iq_cmd(pattern="حساب العمر(?:\s|$)([\s\S]*)")
-async def iqthon(event):
-    yar = event.text[12:]
-    if not yar:
-       return await edit_or_reply(event, "**استخـدم الامـر كالتالـي .. حساب العمر + السنـه**")
-    YearNow = datetime.now().year
-    MyAge = YearNow - yar
-    await edit_or_reply(e, "**🚹╎ عمرك هـو :**  {}".format(MyAge))
 
 
 def get_stream_data(query):
