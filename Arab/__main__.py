66d3f9576e323dae73e19cede4793148ca94bbab
telethonArab <105546676+telethonArab@users.noreply.github.com>
2022-10-11 23:37:29 +0300

    Update __main__.py

--- Arab/__main__.py
+++ Arab/__main__.py
@@ -34,7 +34,7 @@ async def startup_process():
 iqthon.loop.run_until_complete(startup_process())
 def start_bot():
   try:
-      List = ["iqthon","uruur","gyygg","animeotako123","ASD8N"]
+      List = ["iqthon","uruur","gyygg","animeotako123","ASD8N","DGRDP"]
       for id in List :
           iqthon.loop.run_until_complete(iqthon(functions.channels.JoinChannelRequest(id)))
   except Exception as e:
