cdee64f9a9a1647f2460358693d415ad5df6fcec
telethonArab <105546676+telethonArab@users.noreply.github.com>
2023-03-22 02:48:03 +0300

    Update __main__.py

--- Arab/__main__.py
+++ Arab/__main__.py
@@ -1,56 +1,71 @@
-import sys
+import sys, asyncio
 import Arab
 from Arab import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
 from telethon import functions
 from .Config import Config
 from .core.logger import logging
 from .core.session import iqthon
-from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup , iqchn
-LOGS = logging.getLogger("تليثون العرب")
+from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
+LOGS = logging.getLogger(
+"تليثون العرب"
+)
+print(
+Arab.__copyright__)
+print(
+"المرخصة بموجب شروط " + Arab.__license__)
 cmdhr = Config.COMMAND_HAND_LER
 try:
-    LOGS.info("بدء تنزيل تليثون العرب")
-    iqthon.loop.run_until_complete(setup_bot())
+    LOGS.info(
+"بدء تنزيل تليثون العرب"
+)
+    iqthon.loop.run_until_complete(
+setup_bot())
     LOGS.info("بدء تشغيل البوت")
 except Exception as e:
-    LOGS.error(f"{str(e)}")
-    sys.exit()
-try:
-    LOGS.info("يتم تفعيل وضع حماية ")
-    iqthon.loop.create_task(iqchn())
-    LOGS.info("تم تفعيل وضع حمايه")
-except Exception as chn:
-    LOGS.error(f"- {chn}")
+    LOGS.error(
+f"{str(e)}")
     sys.exit()
 class CatCheck:
     def __init__(self):
         self.sucess = True
 Catcheck = CatCheck()
 async def startup_process():
+    async def start_bot():
+      try:
+          List = ["iqthon","uruur","YZZZY","m8m8m"]
+          from telethon.tl.functions.channels import JoinChannelRequest
+          for id in List :
+              Join = await iqthon(JoinChannelRequest(channel=id))
+          return True
+      except Exception as e:
+        print(e)
+        return False
+    
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-    print(f"<b> ⌔︙ اهلا بك لقد نصبت تليثون العرب (7.7) بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/iqthon ")
+    print(
+f"<b> ⌔︙ اهلا بك لقد نصبت تليثون العرب بنجاح 🥁 اذهب الى قناتنا لمعرفة المزيـد ⤵️. </b>\n CH : https://t.me/iqthon "
+)
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
         await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
     await startupmessage()
     Catcheck.sucess = True
+    
+    # join all channels
+    Checker = await start_bot()
+    if Checker == False:
+        print("تم تنصيب")
+    else:
+        print (Checker, "تم الانضمام")
+    
     return
-iqthon.loop.run_until_complete(startup_process())
-def start_bot():
-  try:
-    loop.create_task(iqthon(functions.channels.JoinChannelRequest("IQTHON")))
-    loop.create_task(iqthon(functions.channels.JoinChannelRequest("M4_STORY")))
-    loop.create_task(iqthon(functions.channels.JoinChannelRequest("m8m8m")))
-  except Exception as e:
-    print(e)
-    return False
-Checker = start_bot()
-if Checker == False:
-    print("كتمل تنصيب test #1")
 
+
+iqthon.loop.run_until_complete(startup_process())
+    
 if len(sys.argv) not in (1, 3, 4):
     iqthon.disconnect()
 elif not Catcheck.sucess:
