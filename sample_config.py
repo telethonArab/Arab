3909223fdded15990e9f95bfda749b5b836c9f6b
telethonArab <105546676+telethonArab@users.noreply.github.com>
2023-03-21 16:32:05 +0300

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,84 @@
+import os
+from typing import Set
+class Config(object):
+    LOGGER = True
+    ALIVE_NAME = os.environ.get("ALIVE_NAME", "@IQTHON")
+    APP_ID = int(os.environ.get("APP_ID", 6))
+    API_HASH = os.environ.get("API_HASH") or None
+    DB_URI = os.environ.get("DATABASE_URL", None)
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    STRING_SESSION2 = os.environ.get("STRING_SESSION2", None)
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN") or os.environ.get("TG_BOT_TOKEN_BF_HER", None)
+    TG_BOT_USERNAME = None
+    TZ = os.environ.get("TZ", "Asia/Baghdad")
+    UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/telethonArab/telethonAr1")
+    AUTONAME = os.environ.get("AUTONAME", "@iqthon")
+    PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID") or 0)
+    PRIVATE_GROUP_ID = int(os.environ.get("PRIVATE_GROUP_ID") or 0)
+    PRIVATE_CHANNEL_BOT_API_ID = int(os.environ.get("PRIVATE_CHANNEL_BOT_API_ID") or 0)
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    OWNER_ID = int(os.environ.get("OWNER_ID") or 0)
+    PM_LOGGER_GROUP_ID = int( os.environ.get("PM_LOGGER_GROUP_ID") or os.environ.get("PM_LOGGR_BOT_API_ID") or 0 )
+    PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL") or 0)
+    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "iqthon")
+    THUMB_IMAGE = os.environ.get("THUMB_IMAGE", "https://telegra.ph/file/f1b9e7e1a5b0f719bc5d9.jpg")
+    PORT = os.environ.get("PORT", "8080")
+    NO_LOAD = [x for x in os.environ.get("NO_LOAD", "").split()]
+    DIGITAL_PIC = os.environ.get("DIGITAL_PIC", None)
+    DEFAULT_PIC = os.environ.get("DEFAULT_PIC", None)
+    DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
+    DEFAULT_NAME = os.environ.get("DEFAULT_NAME", None)
+    COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", r".")
+    SUDO_COMMAND_HAND_LER = os.environ.get("SUDO_COMMAND_HAND_LER", r".")
+    TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "downloads")
+    TEMP_DIR = os.environ.get("TEMP_DIR", "./temp/")
+    CHANGE_TIME = int(os.environ.get("CHANGE_TIME", 60))
+    ANTISPAMBOT_BAN = os.environ.get("ANTISPAMBOT_BAN", False)
+    DUAL_LOG = os.environ.get("DUAL_LOG", False)
+    FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR", "▰")
+    UNFINISHED_PROGRESS_STR = os.environ.get("UNFINISHED_PROGRESS_STR", "▱")
+    SCREEN_SHOT_LAYER_ACCESS_KEY = os.environ.get("SCREEN_SHOT_LAYER_ACCESS_KEY", None)
+    OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
+    IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
+    IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
+    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
+    GENIUS_API_TOKEN = os.environ.get("GENIUS_API_TOKEN", None)
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+    CURRENCY_API = os.environ.get("CURRENCY_API", None)
+    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
+    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+    G_DRIVE_FOLDER_ID = os.environ.get("G_DRIVE_FOLDER_ID", None)
+    G_DRIVE_DATA = os.environ.get("G_DRIVE_DATA", None)
+    G_DRIVE_INDEX_LINK = os.environ.get("G_DRIVE_INDEX_LINK", None)
+    TG_2STEP_VERIFICATION_CODE = os.environ.get("TG_2STEP_VERIFICATION_CODE", None)
+    WATCH_COUNTRY = os.environ.get("WATCH_COUNTRY", "IN")
+    BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
+    LASTFM_API = os.environ.get("LASTFM_API", None)
+    LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
+    LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
+    LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
+    SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
+    RANDOM_STUFF_API_KEY = os.environ.get("RANDOM_STUFF_API_KEY", None)
+    GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
+    GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
+    FFMPEG_DOWNLOAD_URL = os.environ.get("FFMPEG_DOWNLOAD_URL", None)
+    DEEP_AI = os.environ.get("DEEP_AI", None)
+    from telethon.tl.types import ChatBannedRights
+    ANTI_FLOOD_WARN_MODE = ChatBannedRights(until_date=None, view_messages=None, send_messages=True)
+    MAX_MESSAGE_SIZE_LIMIT = 4095
+    LOAD = []
+    CHROME_BIN = os.environ.get("CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
+    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/app/.chromedriver/bin/chromedriver")
+    GROUP_REG_SED_EX_BOT_S = os.environ.get("GROUP_REG_SED_EX_BOT_S", r"(regex|moku|BananaButler_|rgx|l4mR)bot")
+    COUNTRY = str(os.environ.get("COUNTRY", ""))
+    TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
+    UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "master")
+    SUDO_USERS: Set[int] = set()
+    CATUBLOGO = None
+    BOTLOG = False
+    BOTLOG_CHATID = 0
+class Production(Config):
+    LOGGER = False
+class Development(Config):
+    LOGGER = True
