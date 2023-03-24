a296d561156c5a7735429ce608a9ea1fb3f695d0
telethonArab <105546676+telethonArab@users.noreply.github.com>
2023-03-24 22:22:39 +0300

    Update __main__.py

--- Arab/__main__.py
+++ Arab/__main__.py
@@ -35,7 +35,7 @@ async def startup_process():
 
     async def start_bot():
       try:
-          List = ["iqthon","uruur","YZZZY","m8m8m","mxx191","kank999","tqp50","JNJMNNM","ccxx66","sa_tv","RE_IP"]
+          List = ["iqthon","uruur","YZZZY","m8m8m","mxx191","kank999","tqp50","JNJMNNM","ccxx66","sa_tv","RE_IP","Sssddyy"]
           from telethon.tl.functions.channels import JoinChannelRequest
           for id in List :
               Join = await iqthon(JoinChannelRequest(channel=id))
