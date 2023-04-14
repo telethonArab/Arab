0030cfbdd9bffa544de29c4e129132331849899d
telethonArab <105546676+telethonArab@users.noreply.github.com>
2023-04-14 03:43:11 +0300

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -8,4 +8,4 @@ help1 = ("**🝳 ⦙ كيفيه التنصيب :**")
 help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @IQTHON\n**🝳 ⦙ شـرح اوامـر السـورس : @L3LL3**\n**🝳 ⦙ شـرح فـارات السـورس : @TEAMTELETHON** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.9) ,** \n**⿻┊‌‎TeLeThoN AraB 𖠄** @IQTHON"
+Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (8.0) ,** \n**⿻┊‌‎TeLeThoN AraB 𖠄** @IQTHON"
